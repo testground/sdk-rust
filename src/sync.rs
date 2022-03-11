4b19f7a203f86f11876414e9d530424b4e5f6281
Max Inden <mail@max-inden.de>
2022-03-11 18:20:56 +0100

    src/sync: Add Client::publish_success

--- src/sync.rs
+++ src/sync.rs
@@ -57,6 +57,7 @@ impl Client {
                 state: contextualized_state,
             }),
             barrier: None,
+            publish: None,
         };
 
         self.send(request).await?;
@@ -83,6 +84,7 @@ impl Client {
         let request = Request {
             id: id.clone(),
             signal_entry: None,
+            publish: None,
             barrier: Some(BarrierRequest {
                 state: contextualized_state,
                 target,
@@ -98,6 +100,36 @@ impl Client {
             return Err(BarrierError::Remote(resp.error));
         }
         Ok(())
+    }
+
+    pub async fn publish_success(&mut self) -> Result<u64, PublishSuccessError> {
+        let id = self.next_id().to_string();
+
+        let request = Request {
+            id: id.clone(),
+            signal_entry: None,
+            barrier: None,
+            publish: Some(PublishRequest {
+                topic: topic(),
+                payload: Event {
+                    success_event: SuccessEvent {
+                        group: std::env::var("TEST_GROUP_ID").unwrap(),
+                    },
+                },
+            }),
+        };
+
+        self.send(request).await?;
+        let resp = self.receive().await?;
+        if resp.id != id {
+            return Err(PublishSuccessError::UnexpectedId(resp.id));
+        }
+        if !resp.error.is_empty() {
+            return Err(PublishSuccessError::Remote(resp.error));
+        }
+        resp.publish
+            .ok_or(PublishSuccessError::ExpectedPublishInResponse)
+            .map(|resp| resp.seq)
     }
 
     fn next_id(&mut self) -> u64 {
@@ -120,14 +152,21 @@ impl Client {
     }
 }
 
-fn contextualize_state(state: String) -> String {
+fn context_from_env() -> String {
     format!(
-        "run:{}:plan:{}:case:{}:states:{}",
+        "run:{}:plan:{}:case:{}",
         std::env::var("TEST_RUN").unwrap(),
         std::env::var("TEST_PLAN").unwrap(),
         std::env::var("TEST_CASE").unwrap(),
-        state
     )
+}
+
+fn contextualize_state(state: String) -> String {
+    format!("{}:states:{}", context_from_env(), state,)
+}
+
+fn topic() -> String {
+    format!("{}:run_events", context_from_env(),)
 }
 
 #[derive(Error, Debug)]
@@ -157,6 +196,20 @@ pub enum BarrierError {
 }
 
 #[derive(Error, Debug)]
+pub enum PublishSuccessError {
+    #[error("Remote returned error {0}.")]
+    Remote(String),
+    #[error("Remote returned response with unexpected ID {0}.")]
+    UnexpectedId(String),
+    #[error("Expected remote to return signal entry in response.")]
+    ExpectedPublishInResponse,
+    #[error("Error sending request {0}")]
+    Send(#[from] SendError),
+    #[error("Error receiving response: {0}")]
+    Receive(#[from] ReceiveError),
+}
+
+#[derive(Error, Debug)]
 pub enum SendError {
     #[error("Soketto: {0}")]
     Soketto(#[from] soketto::connection::Error),
@@ -179,6 +232,7 @@ struct Request {
     id: String,
     signal_entry: Option<SignalEntryRequest>,
     barrier: Option<BarrierRequest>,
+    publish: Option<PublishRequest>,
 }
 
 #[derive(Serialize)]
@@ -192,14 +246,36 @@ struct BarrierRequest {
     target: u64,
 }
 
+#[derive(Serialize)]
+struct PublishRequest {
+    topic: String,
+    payload: Event,
+}
+
+#[derive(Serialize)]
+struct Event {
+    success_event: SuccessEvent,
+}
+
+#[derive(Serialize)]
+struct SuccessEvent {
+    group: String,
+}
+
 #[derive(Deserialize, Debug)]
 struct Response {
     id: String,
     signal_entry: Option<SignalEntryResponse>,
     error: String,
+    publish: Option<PublishResponse>,
 }
 
 #[derive(Deserialize, Debug)]
 struct SignalEntryResponse {
+    seq: u64,
+}
+
+#[derive(Deserialize, Debug)]
+struct PublishResponse {
     seq: u64,
 }
