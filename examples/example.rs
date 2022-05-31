8218ca37b24907ba15cb748c3029dfdb3eab2709
ackintosh <sora.akatsuki@gmail.com>
2022-05-31 10:17:09 +0900

    Add parameter to the example test case

--- examples/example.rs
+++ examples/example.rs
@@ -1,6 +1,11 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (client, _run_parameters) = testground::client::Client::new().await?;
+    let (client, run_parameters) = testground::client::Client::new().await?;
+
+    client.record_message(format!(
+        "{}, sdk-rust!",
+        run_parameters.test_instance_params.get("greeting").unwrap()
+    ));
 
     client.record_success().await?;
 
